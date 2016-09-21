0d167ab5163cb0c5c1e45911dcfdae39fd8cb8c1
Alex Kiura <alex.kiura@andela.com>
2016-09-21 15:12:14 +0300

    [Feature] Add book list view

--- books/views.py
+++ books/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Book, Category
 
-# Create your views here.
+
+class BookListView(ListView):
+    template_name = 'books/book_list.html'
+    model = Book
+
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['books'] = self.get_queryset()
+        return context
+
+    def get_queryset(self):
+        return Book.objects.all()
