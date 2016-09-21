60e168b27b1cee9895d0ef5acbd41c45be5a0cf4
Alex Kiura <alex.kiura@andela.com>
2016-09-21 14:35:00 +0300

    [Feature] Add models
    [Chore] Test book models
    
    [Feature] Create application models

--- books/models.py
+++ books/models.py
@@ -2,4 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=256)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='books')
+
+    def __unicode__(self):
+        return self.name
--- books/tests/test_models.py
+++ books/tests/test_models.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from books.models import Book, Category
 
-# Create your tests here.
+class TestModels(TestCase):
+
+    def setUp(self):
+        self.test_category = Category.objects.create(name='General')
+        self.test_book = Book.objects.create(
+            name='Life', category=self.test_category)
+
+    def test_can_create_categories(self):
+        fiction_category = Category.objects.create(name='Fiction')
+        created_category = Category.objects.filter(name='Fiction').first()
+        self.assertEqual(fiction_category, created_category)
+        self.assertEqual(len(Category.objects.all()), 2)
+
+    def test_can_create_books(self):
+        general_book = Book.objects.create(
+            name='General book', category=self.test_category)
+        created_book = Book.objects.filter(name='General book').first()
+        self.assertEqual(general_book, created_book)
+        self.assertEqual(len(Book.objects.all()), 2)
+
+    def test_can_edit_books(self):
+        life_book = Book.objects.filter(name='Life').first()
+        life_book.name = 'Wiki'
+        life_book.save()
+        self.assertEqual(life_book.name, 'Wiki')
