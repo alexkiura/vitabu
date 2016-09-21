3c372cb5e61519acf5098480c563533cbee861ed
Alex Kiura <alex.kiura@andela.com>
2016-09-21 15:35:31 +0300

    [Chore] Fix errors in urls

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'', views.BookListView, name='book_list'),
+    url(r'', views.BookListView.as_view(), name='book_list'),
 ]
